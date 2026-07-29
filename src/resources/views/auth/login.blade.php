4a6214f4d156f648a07fef4a2dea22375e7bf794
kuma <dorufin1335@gmail.com>
2026-07-30 00:58:37 +0900

    不要なファイルを削除

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,31 +1,32 @@
-<!-- スタッフログイン画面 -->
+<!-- ログイン画面 -->
 @extends('layouts.auth')
 
 @section('title','ログイン')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('/css/auth/login.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/auth/common.css') }}">
 @endsection
 
 @section('content')
 <form action="/login" method="post" class="form center">
     @csrf
-    <h1 class="auth__title">ログイン</h1>
-    <label for="mail" class="entry__name">メールアドレス</label>
-    <input name="email" id="mail" type="email" class="input" value="{{ old('email') }}">
+    <h1 class="auth__title">Login</h1>
+    <label for="mail" class="form__label">メールアドレス</label>
+    <input name="email" id="email" type="email" class="input" value="{{ old('email') }}">
     <div class="form__error">
         @error('email')
         {{ $message }}
         @enderror
     </div>
-    <label for="password" class="entry__name">パスワード</label>
+    <label for="password" class="form__label">パスワード</label>
     <input name="password" id="password" type="password" class="input">
     <div class="form__error">
         @error('password')
         {{ $message }}
         @enderror
     </div>
-    <button class="btn btn--big">ログインする</button>
-    <a href="/register" class="link">会員登録はこちら</a>
+    <button class="btn--main">ログイン</button>
+    <a href="/register" class="btn--sab">新規登録</a>
+    <!-- <a href="/register" class="link">パスワードをお忘れの方はこちら</a> -->
 </form>
 @endsection
